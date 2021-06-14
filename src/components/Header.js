1945863de45ad0d94979edfbe26e06574d6181dd
Yash Bajoria <42349367+yashbajoria@users.noreply.github.com>
2021-06-15 00:45:55 +0530

    update content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,64 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import JSONData from "../content/mycontent.json"
-
+import JSONData from '../content/mycontent.json'
 
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo backdrop-blur">
-      <img src={JSONData.MainImage} style={{"maxWidth": "100%",
-        "maxHeight": "100%",
-        "display": "block",
-        "borderRadius": "50%"
-}}></img>
+      <img
+        src={JSONData.MainImage}
+        style={{
+          maxWidth: '100%',
+          maxHeight: '100%',
+          display: 'block',
+          borderRadius: '50%',
+        }}
+      ></img>
     </div>
     <div className="content backdrop-blur">
       <div className="inner backdrop-blur">
-        <h1>{JSONData.Name}</h1>
+        <h1
+          style={{
+            fontFamily: 'Open Sans, sans-serif',
+            fontWeight: '300',
+          }}
+        >
+          {JSONData.Name}
+        </h1>
+        ____
         <p>
-          {JSONData.Description.map((item) => {
-            return (<div><br/>{item}</div>)
+          {JSONData.Description.map(item => {
+            return (
+              <div
+                style={{
+                  fontFamily: 'Fira Mono, monospace',
+                  fontWeight: '100',
+                  fontSize: 'small',
+                  letterSpacing: '1px',
+                }}
+              >
+                {item}
+              </div>
+            )
           })}
         </p>
       </div>
     </div>
-    <nav className="backdrop-blur">
+    <nav
+      className="backdrop-blur"
+      style={{
+        backgroundColor: 'rgba(255, 255, 255, 0.9)',
+      }}
+    >
       <ul>
         <li>
           <button
             onClick={() => {
               props.onOpenArticle('intro')
+            }}
+            style={{
+              fontFamily: 'Fira Mono, monospace',
+              fontWeight: '200',
             }}
           >
             About Me
@@ -38,6 +69,10 @@ const Header = props => (
             onClick={() => {
               props.onOpenArticle('work')
             }}
+            style={{
+              fontFamily: 'Fira Mono, monospace',
+              fontWeight: '200',
+            }}
           >
             Work
           </button>
@@ -46,6 +81,10 @@ const Header = props => (
           <button
             onClick={() => {
               props.onOpenArticle('about')
+            }}
+            style={{
+              fontFamily: 'Fira Mono, monospace',
+              fontWeight: '200',
             }}
           >
             CV
@@ -56,8 +95,12 @@ const Header = props => (
             onClick={() => {
               props.onOpenArticle('contact')
             }}
+            style={{
+              fontFamily: 'Fira Mono, monospace',
+              fontWeight: '200',
+            }}
           >
-            Contact
+            Connect
           </button>
         </li>
       </ul>
